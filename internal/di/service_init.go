9f06865c04daa2956c46fcb5b13c1ee85f551fbf
fujis <ppdx999@gmail.com>
2025-04-22 14:35:12 +0900

    Refactoring 一貫性向上

--- internal/di/service_init.go
+++ internal/di/service_init.go
@@ -2,30 +2,30 @@ package di
 
 import init_ "github.com/ppdx999/kyopro/internal/service/init"
 
-func InitializeGetWd() init_.GetWd {
-	return InitializeFsImpl()
-}
-
-func InitializeMakePublicDir() init_.MakePublicDir {
-	return InitializeFsImpl()
-}
-
-func InitializeGetProblemIds() init_.GetProblemIds {
-	return InitializeAtcoder()
-}
-
-func InitializeGetWorkspace() init_.GetWorkspace {
-	getWd := InitializeGetWd()
-	return init_.NewGetWorkspaceImpl(getWd)
-}
-
-func InitializeMakeProblemDir() init_.MakeProblemDir {
-	makePublicDir := InitializeMakePublicDir()
-	getWorkspace := InitializeGetWorkspace()
-	return init_.NewMakeProblemDirImpl(makePublicDir, getWorkspace)
-}
-
 func InitializeInitService() init_.InitService {
+	var InitializeGetWd = func() init_.GetWd {
+		return InitializeFsImpl()
+	}
+
+	var InitializeMakePublicDir = func() init_.MakePublicDir {
+		return InitializeFsImpl()
+	}
+
+	var InitializeGetProblemIds = func() init_.GetProblemIds {
+		return InitializeAtcoder()
+	}
+
+	var InitializeGetWorkspace = func() init_.GetWorkspace {
+		getWd := InitializeGetWd()
+		return init_.NewGetWorkspaceImpl(getWd)
+	}
+
+	var InitializeMakeProblemDir = func() init_.MakeProblemDir {
+		makePublicDir := InitializeMakePublicDir()
+		getWorkspace := InitializeGetWorkspace()
+		return init_.NewMakeProblemDirImpl(makePublicDir, getWorkspace)
+	}
+
 	getProblemIds := InitializeGetProblemIds()
 	makeProblemDir := InitializeMakeProblemDir()
 	return init_.NewInitServiceImpl(getProblemIds, makeProblemDir)
