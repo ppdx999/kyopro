15cf2251f04941b059855937a2e58fec7e73797f
fujis <ppdx999@gmail.com>
2025-04-27 16:32:33 +0900

    コードの一貫性を強化(cli)

--- internal/presentation/cli/dispatcher_test.go
+++ internal/presentation/cli/dispatcher_test.go
@@ -9,37 +9,117 @@ import (
 )
 
 func TestRun(t *testing.T) {
+	type mock struct {
+		sub1 *MockCmd
+		sub2 *MockCmd
+	}
 	tests := []struct {
-		name      string
-		args      []string
-		want      cli.ExitCode
-		calledCmd string
+		name string
+		args []string
+		mock func(c *gomock.Controller) *mock
+		want cli.ExitCode
 	}{
 		{
-			name:      "Correct Case Call sub1",
-			args:      []string{"sub1"},
-			want:      cli.ExitOK,
-			calledCmd: "sub1",
+			name: "Correct Case Call sub1",
+			args: []string{"sub1"},
+			mock: func(c *gomock.Controller) *mock {
+				return &mock{
+					sub1: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub1")
+						m.EXPECT().Run(gomock.Any()).Return(cli.ExitOK)
+						return m
+					}(),
+					sub2: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub2")
+						return m
+					}(),
+				}
+			},
+			want: cli.ExitOK,
 		},
 		{
-			name:      "Correct Case Call sub2",
-			args:      []string{"sub2"},
-			want:      cli.ExitOK,
-			calledCmd: "sub2",
+			name: "Correct Case Call sub2",
+			args: []string{"sub2"},
+			want: cli.ExitOK,
+			mock: func(c *gomock.Controller) *mock {
+				return &mock{
+					sub1: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub1")
+						return m
+					}(),
+					sub2: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub2")
+						m.EXPECT().Run(gomock.Any()).Return(cli.ExitOK)
+						return m
+					}(),
+				}
+			},
 		},
 		{
-			name: "No subcommand provided",
+			name: "No subcommand args provided",
 			args: nil,
+			mock: func(c *gomock.Controller) *mock {
+				return &mock{
+					sub1: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub1")
+						m.EXPECT().Description().Return("sub1 description")
+						return m
+					}(),
+					sub2: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub2")
+						m.EXPECT().Description().Return("sub2 description")
+						return m
+					}(),
+				}
+			},
 			want: cli.ExitErr,
 		},
 		{
 			name: "Unknown subcommand",
 			args: []string{"unknown"},
+			mock: func(c *gomock.Controller) *mock {
+				return &mock{
+					sub1: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub1")
+						m.EXPECT().Description().Return("sub1 description")
+						return m
+					}(),
+					sub2: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub2")
+						m.EXPECT().Description().Return("sub2 description")
+						return m
+					}(),
+				}
+			},
 			want: cli.ExitErr,
 		},
 		{
 			name: "Help flag",
 			args: []string{"-h"},
+			mock: func(c *gomock.Controller) *mock {
+				return &mock{
+					sub1: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub1")
+						m.EXPECT().Description().Return("sub1 description")
+						return m
+					}(),
+					sub2: func() *MockCmd {
+						m := NewMockCmd(c)
+						m.EXPECT().Name().Return("sub2")
+						m.EXPECT().Description().Return("sub2 description")
+						return m
+					}(),
+				}
+			},
 			want: cli.ExitErr,
 		},
 	}
@@ -47,43 +127,24 @@ func TestRun(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// Arrange
-			var calledCmd string
 			mockCtrl := gomock.NewController(t)
 			defer mockCtrl.Finish()
-
-			sub1 := NewMockCmd(mockCtrl)
-			sub1.EXPECT().Name().Return("sub1")
-			sub1.EXPECT().Description().Return("this is sub1 description")
-			sub1.EXPECT().Run(gomock.Any()).DoAndReturn(func(args []string) cli.ExitCode {
-				calledCmd = "sub1"
-				return cli.ExitOK
-			})
-
-			sub2 := NewMockCmd(mockCtrl)
-			sub2.EXPECT().Name().Return("sub2")
-			sub2.EXPECT().Description().Return("this is sub2 description")
-			sub2.EXPECT().Run(gomock.Any()).DoAndReturn(func(args []string) cli.ExitCode {
-				calledCmd = "sub2"
-				return cli.ExitOK
-			})
+			mock := tt.mock(mockCtrl)
 
 			msgSender := user_mock.NewMockMsgSender(mockCtrl)
-			msgSender.EXPECT().SendMsg(gomock.Any())
+			msgSender.EXPECT().SendMsg(gomock.Any()).AnyTimes()
 
 			root := cli.NewDispatcher(msgSender)
-			root.Register(sub1)
-			root.Register(sub2)
+			root.Register(mock.sub1)
+			root.Register(mock.sub2)
 
 			// Act
 			got := root.Run(tt.args)
 
+			// Assert
 			if got != tt.want {
 				t.Errorf("Run() = %v, want %v", got, tt.want)
 				return
-			}
-
-			if calledCmd != tt.calledCmd {
-				t.Errorf("%s was not called", tt.calledCmd)
 			}
 		})
 	}
