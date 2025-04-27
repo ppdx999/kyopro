648e0a32e5e3cfbc7725c4f3b1e0f506a0dd3c84
fujis <ppdx999@gmail.com>
2025-04-27 16:06:47 +0900

    コードの一貫性を強化(TestCase)

--- internal/domain/service/testcase/saver_test.go
+++ internal/domain/service/testcase/saver_test.go
@@ -1,7 +1,7 @@
 package testcase_test
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
@@ -14,21 +14,16 @@ func TestSaveTestCase(t *testing.T) {
 		tc *model.TestCase
 	}
 	type mock struct {
-		getWd              string
-		getWdErr           error
-		makePublicDirErr   error
-		writePublicFileErr error
-	}
-	type want struct {
-		writtenFiles []string
-		err          bool
+		getWd            *MockGetWd
+		publicDirMaker   *MockPublicDirMaker
+		publicFileWriter *MockPublicFileWriter
 	}
 
 	tests := []struct {
-		name string
-		args *args
-		mock *mock
-		want *want
+		name    string
+		args    *args
+		mock    func(c *gomock.Controller) *mock
+		wantErr bool
 	}{
 		{
 			name: "success",
@@ -37,67 +32,130 @@ func TestSaveTestCase(t *testing.T) {
 				Input: []byte("input 1.in"),
 				Want:  []byte("output 1.out"),
 			}},
-			mock: &mock{
-				getWd: "/tmp",
+			mock: func(c *gomock.Controller) *mock {
+				return &mock{
+					getWd: func() *MockGetWd {
+						m := NewMockGetWd(c)
+						m.EXPECT().GetWd().Return("/tmp", nil)
+						return m
+					}(),
+					publicDirMaker: func() *MockPublicDirMaker {
+						m := NewMockPublicDirMaker(c)
+						m.EXPECT().MakePublicDir("/tmp/test").Return(nil)
+						return m
+					}(),
+					publicFileWriter: func() *MockPublicFileWriter {
+						m := NewMockPublicFileWriter(c)
+						m.EXPECT().WritePublicFile("/tmp/test/1.in", []byte("input 1.in")).Return(nil)
+						m.EXPECT().WritePublicFile("/tmp/test/1.out", []byte("output 1.out")).Return(nil)
+						return m
+					}(),
+				}
 			},
 		},
 		{
 			name: "getWd error",
-			args: &args{tc: &model.TestCase{ID: "1"}},
-			mock: &mock{getWdErr: fmt.Errorf("getWd error")},
-			want: &want{err: true},
+			args: &args{tc: &model.TestCase{
+				ID:    "1",
+				Input: []byte("input 1.in"),
+				Want:  []byte("output 1.out"),
+			}},
+			mock: func(c *gomock.Controller) *mock {
+				return &mock{
+					getWd: func() *MockGetWd {
+						m := NewMockGetWd(c)
+						m.EXPECT().GetWd().Return("", errors.New("getWd error"))
+						return m
+					}(),
+					publicDirMaker: func() *MockPublicDirMaker {
+						m := NewMockPublicDirMaker(c)
+						return m
+					}(),
+					publicFileWriter: func() *MockPublicFileWriter {
+						m := NewMockPublicFileWriter(c)
+						return m
+					}(),
+				}
+			},
+			wantErr: true,
 		},
 		{
 			name: "makePublicDir error",
-			args: &args{tc: &model.TestCase{ID: "1"}},
-			mock: &mock{
-				getWd:            "/tmp",
-				makePublicDirErr: fmt.Errorf("makePublicDir error"),
+			args: &args{tc: &model.TestCase{
+				ID:    "1",
+				Input: []byte("input 1.in"),
+				Want:  []byte("output 1.out"),
+			}},
+			mock: func(c *gomock.Controller) *mock {
+				return &mock{
+					getWd: func() *MockGetWd {
+						m := NewMockGetWd(c)
+						m.EXPECT().GetWd().Return("/tmp", nil)
+						return m
+					}(),
+					publicDirMaker: func() *MockPublicDirMaker {
+						m := NewMockPublicDirMaker(c)
+						m.EXPECT().MakePublicDir("/tmp/test").Return(errors.New("makePublicDir error"))
+						return m
+					}(),
+					publicFileWriter: func() *MockPublicFileWriter {
+						m := NewMockPublicFileWriter(c)
+						return m
+					}(),
+				}
 			},
-			want: &want{err: true},
+			wantErr: true,
 		},
 		{
 			name: "writePublicFile error",
-			args: &args{tc: &model.TestCase{ID: "1"}},
-			mock: &mock{
-				getWd:              "/tmp",
-				writePublicFileErr: fmt.Errorf("writePublicFile error"),
+			args: &args{tc: &model.TestCase{
+				ID:    "1",
+				Input: []byte("input 1.in"),
+				Want:  []byte("output 1.out"),
+			}},
+			mock: func(c *gomock.Controller) *mock {
+				return &mock{
+					getWd: func() *MockGetWd {
+						m := NewMockGetWd(c)
+						m.EXPECT().GetWd().Return("/tmp", nil)
+						return m
+					}(),
+					publicDirMaker: func() *MockPublicDirMaker {
+						m := NewMockPublicDirMaker(c)
+						m.EXPECT().MakePublicDir("/tmp/test").Return(nil)
+						return m
+					}(),
+					publicFileWriter: func() *MockPublicFileWriter {
+						m := NewMockPublicFileWriter(c)
+						m.EXPECT().WritePublicFile("/tmp/test/1.in", []byte("input 1.in")).Return(errors.New("writePublicFile error"))
+						m.EXPECT().WritePublicFile("/tmp/test/1.out", []byte("output 1.out")).Return(nil)
+						return m
+					}(),
+				}
 			},
-			want: &want{err: true},
+			wantErr: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// Arrange
-			var writtenFiles map[string]bool
 			mockCtrl := gomock.NewController(t)
 			defer mockCtrl.Finish()
-			mockGetWd := NewMockGetWd(mockCtrl)
-			mockGetWd.EXPECT().GetWd().Return(tt.mock.getWd, tt.mock.getWdErr)
-			mockDirMaker := NewMockPublicDirMaker(mockCtrl)
-			mockDirMaker.EXPECT().MakePublicDir(gomock.Any()).Return(tt.mock.makePublicDirErr)
-			mockPublicFileWriter := NewMockPublicFileWriter(mockCtrl)
-			mockPublicFileWriter.EXPECT().
-				WritePublicFile(gomock.Any(), gomock.Any()).AnyTimes().
-				Do(func(path string, data []byte) {
-					writtenFiles[path] = true
-				}).Return(tt.mock.writePublicFileErr)
+			mock := tt.mock(mockCtrl)
 
-			saver := testcase.NewTestCaseFsSaver(mockGetWd, mockDirMaker, mockPublicFileWriter)
+			saver := testcase.NewTestCaseFsSaver(
+				mock.getWd,
+				mock.publicDirMaker,
+				mock.publicFileWriter,
+			)
 
 			// Act
 			err := saver.SaveTestCase(tt.args.tc)
 
 			// Assert
-			if (err != nil) != tt.want.err {
-				t.Errorf("TestCaseFsSaver.Save() error = %v, wantErr %v", err, tt.want.err)
-			}
-
-			for _, path := range tt.want.writtenFiles {
-				if !writtenFiles[path] {
-					t.Errorf("TestCaseFsSaver.Save() did not write file %s", path)
-				}
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TestCaseFsSaver.Save() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
